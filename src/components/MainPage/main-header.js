708ef3fe55d8b7a1651b5fcd2e8932f34df6bb21
Pierre Anisimov <anisim_pierre@outlook.com>
2020-10-20 15:09:06 +0100

    started working on the main page

---
+++ src/components/MainPage/main-header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import ButtonAppBar from './main-appbar';
+
+export default function MainHeader(){
+    return(
+        <ButtonAppBar/>
+    )
+}
---
+++ src/pages/MainPage.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import MainHeader from '../components/MainPage/main-header';
+
+export default function FirstPage() {
+    return(
+        <MainPage/>
+    )
+}
