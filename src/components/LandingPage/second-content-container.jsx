6afefa422bcd9f8b13c0e868bc83ef1cf69ab883
Pierre Anisimov <pierreanisimov@Pierres-MacBook-Pro.local>
2020-11-27 00:23:30 +0000

    first content container animation

--- src/components/LandingPage/second-content-container.jsx
+++ src/components/LandingPage/second-content-container.jsx
@@ -2,35 +2,33 @@ import React from "react";
 import { Box, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/styles";
 import "./first-content-style.scss";
-import { Transform } from "@material-ui/icons";
 
 const useStyles = makeStyles((theme) => ({
     MainContainer: {
         width: '100%',
-        // border: '3px solid red',
-        height: '120vh',
+        border: '3px solid red',
+        height: '100vh',
         display: 'flex',
         backgroundColor: '#dedddb',
     },
     ContentContainer: {
-        // border: '3px solid blue',
-        width: '100%',
-        height: '75vh',
+        border: '3px solid blue',
+        width: '70%',
+        height: '65vh',
         margin: 'auto',
         display: 'flex',
         alignContent: 'center',
         flexDirection: 'column',
         flexWrap: 'wrap', 
-        justifyContent: 'center',
     },
     TitleBox: {
         width: '100%',
-        height: '15%',
-        // border: '5px solid red',
+        height: '20%',
+        border: '5px solid red',
     },
     Title:{
         fontSize: '2rem',
-        // border: '3px solid green',
+        border: '3px solid green',
         height: '100%',
         width: '100%',
         padding: '2rem',
@@ -38,29 +36,18 @@ const useStyles = makeStyles((theme) => ({
 
     },
     DescriptionBox: {
-        width: '60%',
-        height: '85%',
-        margin: 'auto',
-        // border: '3px solid',
+        width: '100%',
+        height: '70%',
+        border: '3px solid',
         display: 'flex',
         justifyContent: 'center',
         alignContent: 'center',
-        backgroundColor: 'rgb(169,44,222,0.2)',
-        transformOrigin: 'top left',
-        // transform: 'skewY(5deg)',
-        // boxShadow: '0px 15px 70px #666666',
-        // background: 'rgb(169,44,222)',
-        // background: 'radial-gradient(circle, rgba(169,44,222,1) 0%, rgba(169,44,222,0.14469537815126055) 100%)',
-        // background: 'rgb(169,44,222)',
-        // background: 'radial-gradient(circle, rgba(169,44,222,1) 0%, rgba(169,44,222,0.06906512605042014) 27%)',
-        // background: 'rgb(169,44,222)',
-        // background: 'radial-gradient(circle, rgba(169,44,222,1) 20%, rgba(222,221,219,1) 47%)',
-        background: 'rgb(169,44,222)',
-        background: 'radial-gradient(circle, rgba(169,44,222,0.5256477591036415) 15%, rgba(222,221,219,1) 45%)',
     },
     Description: {
-        // border: '6px solid green',
-        height: '80%',
+        fontSize: '1.8rem',
+        fontFamily: 'Brass Mono Comfortable',
+        border: '6px solid green',
+        height: '100%',
     },
     RetroFont: {
         fontFamily: 'Retro Gaming',
@@ -78,12 +65,11 @@ function FirstContentContainer() {
             </Box>
             <Box className={classes.DescriptionBox}>
               <Typography variant="h4" className={classes.Description} className="Description"
-              style={{fontFamily: 'Brass Mono Comfortable', color: '#fff', backgroundColor: '#4a4949', fontSize: '.7rem',
-              margin: 'auto', minWidth: '300px',}}>
+              style={{fontFamily: 'Brass Mono Comfortable'}}>
               Welcome to <span className={classes.RetroFont}>Spaciator Unleashed !</span> <br></br>
               We are Nitrate Studios aka. <span className={classes.RetroFont} className="glow-2">NO3</span> <br></br>
               Currently developing a single player game. <br></br>
-              Find a trailer down below <span className={classes.RetroFont}>:)</span>  
+              Find a trailer down below <span className={classes.RetroFont} className="glow-3">:)</span>  
               </Typography>
             </Box>
         </Box>
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import HeaderAndTitle from '../components/main-header';
-import FirstContentContainer from '../components/LandingPage/first-content-component'
+import FirstContentContainer from '../components/LandingPage/first-content-component';
+import SecondContentContainer from '../components/LandingPage/second-content-container';
 
 export default function FirstPage() {
     return(
         <div>
             <HeaderAndTitle/>
             <FirstContentContainer/>
+            <SecondContentContainer/>
         </div>
         )
 }
