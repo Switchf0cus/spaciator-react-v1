293d2313f8657527929bebe3483f109bfa4fd44d
Pierre Anisimov <pierreanisimov@Pierres-MacBook-Pro.local>
2020-12-07 17:27:56 +0000

    added trailer

--- src/components/LandingPage/first-content.jsx
+++ src/components/LandingPage/first-content.jsx
@@ -2,11 +2,8 @@ import React from "react";
 import { Box, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/styles";
 import "./first-content-style.scss";
-import { Transform } from "@material-ui/icons";
-import SecondContent from "./second-content";
-import SimpleAccordion from './theThreeWs-component';
-import FooterCoin from './footer-coin';
-import SimpleModal from './modal-component';
+import SimpleAccordion from "./theThreeWs-component";
+import FooterCoin from "./footer-coin";
 
 const useStyles = makeStyles((theme) => ({
   ContentContainer: {
@@ -58,19 +55,22 @@ const useStyles = makeStyles((theme) => ({
     color: "#dac22c",
   },
   ThreeContentsContainer: {
-    width: '100%',
-    height: '100%',
+    width: "100%",
+    height: "100%",
   },
   ThreeContents: {
-    width: '70%',
-    height: 'auto',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    boxSizing: 'border-box',
-    margin: 'auto',
-    paddingTop: '15%',
-    paddingBottom: '10%',
+    width: "70%",
+    height: "auto",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    boxSizing: "border-box",
+    margin: "auto",
+    paddingTop: "15%",
+    paddingBottom: "10%",
+  },
+  trailerDiv: {
+    margin: "auto",
   },
 }));
 
@@ -137,6 +137,37 @@ function FirstContent() {
               className="Title-2"
               style={{ fontFamily: "Retro Gaming", color: "#dac22c" }}
             >
+              Latest Trailer
+            </Typography>
+          </Box>
+          <Box className={classes.DescriptionBox}>
+            <div className={classes.trailerDiv}>
+              <iframe
+                width="960"
+                height="615"
+                src="https://www.youtube.com/embed/hLbNd9SyoQo"
+                frameBorder="1"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              ></iframe>
+            </div>
+          </Box>
+        </Box>
+        <Box
+          component="div"
+          className={classes.ContentContainer}
+          className="ContentContainer-2"
+          style={{
+            height: "100vh",
+          }}
+        >
+          <Box className={classes.TitleBox}>
+            <Typography
+              variant="h3"
+              className={classes.Title}
+              className="Title-2"
+              style={{ fontFamily: "Retro Gaming", color: "#dac22c" }}
+            >
               When?
             </Typography>
           </Box>
@@ -159,21 +190,24 @@ function FirstContent() {
               <br></br>
               However, in the coming days we are sending out{" "}
               <span className="glow-">alpha</span> versions of the game to a{" "}
-              <span className="glow-">limited number of people.</span>{" "}
-              <br></br>
+              <span className="glow-">limited number of people.</span> <br></br>
               If you would like to know more, sign up to our{" "}
               <span className="glow-">newsletter</span> <span>:)</span>
             </Typography>
           </Box>
         </Box>
-        <Box component="div" className={classes.ThreeContentsContainer} className="ThreeContentsContainer">
+
+        <Box
+          component="div"
+          className={classes.ThreeContentsContainer}
+          className="ThreeContentsContainer"
+        >
           <Box component="div" className={classes.ThreeContents}>
-            <SimpleAccordion/>
+            <SimpleAccordion />
             {/* <SimpleModal/> */}
-            </Box>
-           
+          </Box>
         </Box>
-         <FooterCoin/>
+        <FooterCoin />
       </Box>
     </Box>
   );
