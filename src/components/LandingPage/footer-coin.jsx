2d2c7a6538d235fe006534dfec080affacd2a96a
Pierre Anisimov <pierreanisimov@Pierres-MacBook-Pro.local>
2020-12-07 20:26:55 +0000

    added social media icons

--- src/components/LandingPage/footer-coin.jsx
+++ src/components/LandingPage/footer-coin.jsx
@@ -8,23 +8,35 @@ import FacebookIcon from '@material-ui/icons/Facebook';
 
 const useStyles = makeStyles((theme) => ({
     root: {
-  "& .MuiSvgIcon-root": {
-      color: '#a92cde',
-      fontSize: '2rem',
-  }
-},
+        "& .MuiSvgIcon-root": {
+            color: '#a92cde',
+            fontSize: '2.5rem',
+            margin: '1rem',
+             "& :hover": {
+                color: '#dac22c',
+                transform: 'scale(1.1)',
+                transition: '300ms'
+            }
+        },
+        "&.MuiBox-root-51": {
+            width: '40%',
+            height: '100%',
+            margin: 'auto',
+            paddingTop: '3rem',
+           
+        }
+    },
   FooterContainer: {
     width: "100%",
     height: "20vh",
     backgroundColor: "#4a4949",
   },
-  SocialMediaIcons: {
-      width: '40%',
-      height: '100%',
-      margin: 'auto',
-      padding: '4rem',
-      paddingLeft: ''
-  },
+//   SocialMediaIcons: {
+//       width: '40%',
+//       height: '100%',
+//       margin: 'auto',
+//       paddingTop: '2rem',
+//   },
   
 }));
 
@@ -33,11 +45,10 @@ function FooterCoin() {
 
   return (
     <Box component="div" className={classes.FooterContainer}>
-      {/* <CoinButton disabled/> */}
-      <Box component="div" className={classes.SocialMediaIcons}>
-          <TwitterIcon href="https://twitter.com/NitrateStudios" style={{color: '#a92dce', fontSize: '2.5rem'}}/>
-          <YouTubeIcon href="https://www.youtube.com/channel/UCgbvrA3LnzTYbX4sX0K3XJw" style={{color: '#a92dce', fontSize: '2.5rem'}}/>
-          <FacebookIcon href="https://www.facebook.com/Nitrate-Studios-107845914479478" style={{color: '#a92dce', fontSize: '2.5rem'}}/>
+      <Box component="div" className={classes.root}>
+          <TwitterIcon onClick={event => window.location.href='https://twitter.com/NitrateStudios'} />
+          <YouTubeIcon onClick={event => window.location.href='https://www.youtube.com/channel/UCgbvrA3LnzTYbX4sX0K3XJw'} />
+          <FacebookIcon onClick={event => window.location.href='https://www.facebook.com/Nitrate-Studios-107845914479478'} />
       </Box>
       
     </Box>
@@ -45,3 +56,6 @@ function FooterCoin() {
 }
 
 export default FooterCoin;
+
+
+// style={{color: '#a92dce', fontSize: '2.5rem', cursor:'pointer'}}
